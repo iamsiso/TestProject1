2ec08a4c39699b6e36ed2c00e3174f543f54e409
ufo20020427 <357378979@qq.com>
2014-05-22 13:48:39 +0800

    2014052201
    2014052201

---
+++ Report.sql
@@ -0,0 +1,16 @@
+with temp as(
+select distinct ReportMainId from ReportFlow t
+where t.reportmainid in (select id from reportmain where flowmainid = 3 )
+and t.flowdetailid = 0
+),
+temp2 as(
+select reportmainid,count(Id) as n from ReportFlow where reportmainid in
+(select reportmainid from temp)
+group by reportmainid
+having count(Id) <4
+)
+
+
+--update  ReportMain set CurrentFlowDetailId = 10 where  id in(select reportmainid from temp2)
+
+--delete from reportflow where reportmainid in(select reportmainid from temp2) and (flowdetailid=0 or flowdetailid = 12)
